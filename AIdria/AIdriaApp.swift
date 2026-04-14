9593b96e456f8d3dd1eef41f4f9f002433698638
Deandre Medrano <medrano84deandre@gmail.com>
2026-04-13 17:43:26 -0700

    Initial Commit

---
+++ AIdria/AIdriaApp.swift
@@ -0,0 +1,17 @@
+//
+//  AIdriaApp.swift
+//  AIdria
+//
+//  Created by Deandre Medrano on 4/13/26.
+//
+
+import SwiftUI
+
+@main
+struct AIdriaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
